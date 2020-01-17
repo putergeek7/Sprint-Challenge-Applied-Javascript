3fe760c4250dd5c5984c620722d534c3a80588a3
Pat D <49631522+putergeek7@users.noreply.github.com>
2020-01-17 10:34:16 -0800

    working on cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,11 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios
+	.get('https://lambda-times-backend.herokuapp.com/articles')
+	.then(response => {
+		console.log(response);
+	});
+
+const topCardContainer = document.querySelector('.cards-container');
